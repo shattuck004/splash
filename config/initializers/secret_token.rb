382f33b11834ba480bfe77a9810981790e145dfc
Shattuck004 <katie.markov@gmail.com>
2013-01-09 22:42:58 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splash::Application.config.secret_token = 'd5fecd225aef4734049b29a72266997ecf158a7151e617e27c79efd7f2f39a2f5fc6d6daf050c4a17fbb1c8daac06afb92b77e0c13c911d15239d67cbf6f0e9b'
